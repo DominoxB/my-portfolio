b5f3a69a321f22ce90a845b1391745e189b1e427
DominoxB <dominika-biedulska@tlen.pl>
2023-07-12 12:11:01 +0200

    feat: add icons and component with technologies

---
+++ src/components/MyTechnologies.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-20 mx-center">
+    <div class="flex gap-10">
+    <img src="@/icons/html.png" alt="html" class="w-12 h-12"/>
+    <img src="@/icons/css.png" alt="css" class="w-12 h-12"/>
+    <img src="@/icons/javascript.png" alt="js" class="w-12 h-12"/>
+    <img src="@/icons/typescript.png" alt="ts" class="w-12 h-12"/>
+  </div>
+  <div class="flex gap-10 mt-8">
+    <img src="@/icons/vue.png" alt="vue" class="w-12 h-12"/>
+    <img src="@/icons/tailwind.png" alt="tailwind" class="w-12 h-12"/>
+    <img src="@/icons/jest.png" alt="testjest" class="w-12 h-12"/>
+    <img src="@/icons/git.png" alt="git" class="w-12 h-12"/>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTechnologies'
+}
+</script>
