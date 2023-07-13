d1e083d6cb9c8b7e95ede68da8878bd6819a5e14
DominoxB <dominika-biedulska@tlen.pl>
2023-07-13 12:21:08 +0200

    style: styling

--- src/components/MyTechnologies.vue
+++ src/components/MyTechnologies.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="mt-20 mx-center">
     <div class="flex gap-10">
-    <img src="@/icons/html.png" alt="html" class="w-12 h-12"/>
-    <img src="@/icons/css.png" alt="css" class="w-12 h-12"/>
-    <img src="@/icons/javascript.png" alt="js" class="w-12 h-12"/>
-    <img src="@/icons/typescript.png" alt="ts" class="w-12 h-12"/>
-  </div>
+      <img src="@/icons/html.png" alt="html" class="w-12 h-12" />
+      <img src="@/icons/css.png" alt="css" class="w-12 h-12" />
+      <img src="@/icons/javascript.png" alt="js" class="w-12 h-12" />
+      <img src="@/icons/typescript.png" alt="ts" class="w-12 h-12" />
+    </div>
   <div class="flex gap-10 mt-8">
-    <img src="@/icons/vue.png" alt="vue" class="w-12 h-12"/>
-    <img src="@/icons/tailwind.png" alt="tailwind" class="w-12 h-12"/>
-    <img src="@/icons/jest.png" alt="testjest" class="w-12 h-12"/>
-    <img src="@/icons/git.png" alt="git" class="w-12 h-12"/>
+    <img src="@/icons/vue.png" alt="vue" class="w-12 h-12" />
+    <img src="@/icons/tailwind.png" alt="tailwind" class="w-12 h-12" />
+    <img src="@/icons/jest.png" alt="testjest" class="w-12 h-12" />
+    <img src="@/icons/git.png" alt="git" class="w-12 h-12" />
   </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MyTechnologies'
+  name: 'MyTechnologies',
+  setup() {
+    return {
+    }
+  }
 }
 </script>
