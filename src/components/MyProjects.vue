127eb6f8d4101801366105b861092e7164fc165d
DominoxB <dominika-biedulska@tlen.pl>
2023-07-18 14:45:06 +0200

    fix: fix styles

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="text-white mt-20">
-    <span class="uppercase font-semibold text-2xl">{{ $t('projects') }}</span>
+  <div class="mt-20 text-white">
+    <span class="uppercase font-semibold text-2xl text-white">{{ $t('projects') }}</span>
     <div class="relative group">
       <img src="@/images/donutino.png" alt="donutino"
-        class="rounded-md border border-gray-400 mt-2 group-hover:opacity-30 transition duration-700" />
+        class="rounded-md border border-gray-400 mt-2 opacity-50 md:opacity-100 md:group-hover:opacity-30 transition duration-700" />
       <a href="https://dominoxb.github.io/donutshop/" target="_blank">
         <div
-          class="p-8 opacity-0 group-hover:opacity-100 transition duration-700 absolute bottom-0 text-center bg-gray-200 bg-opacity-30 text-gray-50 rounded-b-md">
+          class="p-2 md:p-8 md:opacity-0 opacity-90 md:group-hover:opacity-100 transition duration-700 absolute bottom-0 text-center bg-gray-400 bg-opacity-20 md:bg-gray-200 md:bg-opacity-30 text-white rounded-b-md">
           <span class="font-bold">{{ $t('shopName') }}</span>
           <p class="text-xs font-medium mt-1">{{ $t('shopDesc') }}
           </p>
@@ -15,9 +15,9 @@
     </div>
     <div class="relative group mt-12">
       <img src="@/images/znananiania.png" alt="zn"
-        class="rounded-md border border-gray-400 mt-2 group-hover:opacity-30 transition duration-700" />
+        class="rounded-md border border-gray-400 mt-2 opacity-50 md:opacity-100 md:group-hover:opacity-30 transition duration-700" />
       <div
-        class="p-8 opacity-0 group-hover:opacity-100 transition duration-700 absolute bottom-0 text-center bg-gray-200 bg-opacity-30 text-gray-50 rounded-b-md">
+        class="p-2 md:p-8 md:opacity-0 opacity-90 md:group-hover:opacity-100 transition duration-700 absolute bottom-0 text-center bg-gray-400 bg-opacity-20 md:bg-gray-200 md:bg-opacity-30 rounded-b-md">
         <span class="font-bold">{{ $t('nanny') }}</span>
         <p class="text-xs mt-1">{{ $t('nannyDesc') }}
         </p>
@@ -25,9 +25,9 @@
     </div>
     <div class="relative group mt-12">
       <img src="@/images/games.jpg" alt="games"
-        class="rounded-md border border-gray-400 mt-2 group-hover:opacity-30 transition duration-700" />
+        class="rounded-md border border-gray-400 mt-2 opacity-50 md:opacity-100 md:group-hover:opacity-30 transition duration-700" />
       <div
-        class="p-8 opacity-0 group-hover:opacity-100 transition duration-700 absolute bottom-0 text-center bg-gray-200 bg-opacity-30 text-gray-50 rounded-b-md">
+        class="p-2 md:p-8 md:opacity-0 opacity-90 md:group-hover:opacity-100 transition duration-700 absolute bottom-0 text-center bg-gray-400 bg-opacity-20 md:bg-gray-200 md:bg-opacity-30 rounded-b-md">
         <span class="font-bold">{{ $t('gameProject') }}</span>
         <p class="text-xs font-medium mt-1">{{ $t('gameProjectDesc') }}
         </p>
--- src/components/MyTechnologies.vue
+++ src/components/MyTechnologies.vue
@@ -6,12 +6,12 @@
       <img src="@/icons/javascript.png" alt="js" class="w-8 h-8 md:w-12 md:h-12" />
       <img src="@/icons/typescript.png" alt="ts" class="w-8 h-8 md:w-12 md:h-12" />
     </div>
-  <div class="flex justify-center md:justify-start gap-6 md:gap-10 mt-8">
-    <img src="@/icons/vue.png" alt="vue" class="w-8 h-8 md:w-12 md:h-12" />
-    <img src="@/icons/tailwind.png" alt="tailwind" class="w-8 h-8 md:w-12 md:h-12" />
-    <img src="@/icons/jest.png" alt="testjest" class="w-8 h-8 md:w-12 md:h-12" />
-    <img src="@/icons/git.png" alt="git" class="w-8 h-8 md:w-12 md:h-12" />
-  </div>
+    <div class="flex justify-center md:justify-start gap-6 md:gap-10 mt-8">
+      <img src="@/icons/vue.png" alt="vue" class="w-8 h-8 md:w-12 md:h-12" />
+      <img src="@/icons/tailwind.png" alt="tailwind" class="w-8 h-8 md:w-12 md:h-12" />
+      <img src="@/icons/jest.png" alt="testjest" class="w-8 h-8 md:w-12 md:h-12" />
+      <img src="@/icons/git.png" alt="git" class="w-8 h-8 md:w-12 md:h-12" />
+    </div>
   </div>
 </template>
 
