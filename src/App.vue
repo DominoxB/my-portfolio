33844565fb801c7e7ea57d4693701d8001cb02f0
DominoxB <dominika-biedulska@tlen.pl>
2023-07-18 13:46:01 +0200

    feat: add mobile socials

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="charger" class="sticky top-2 mr-2 mt-2 font-roboto text-white flex justify-end">
+  <div class="flex justify-end gap-10">
+    <MySocialsMobile />
+  <div id="charger" class="sticky top-0 mr-2 mt-8 font-roboto text-white flex justify-end">
     <select v-model="$i18n.locale" class="cursor-pointer bg-transparent">
       <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
         {{ locale }}
       </option>
     </select>
   </div>
-  <MySocialsMobile />
+</div>
   <div class="m-10 md:grid md:grid-cols-2 md:m-40 font-roboto">
     <div class="text-center md:text-start md:sticky md:top-40 md:h-[calc(100vh-547px)]">
       <NameAndProfession />
--- src/components/MySocialsMobile.vue
+++ src/components/MySocialsMobile.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-center gap-4 mt-2 md:hidden">
+  <div class="flex gap-4 mt-8 md:hidden">
     <a href="https://github.com/DominoxB" target="_blank">
-      <img src="@/icons/github.svg" alt="github" class="bg-white rounded w-6 h-6" />
+      <img src="@/icons/github-white.png" alt="github" class="w-6 h-6" />
     </a>
     <a href="https://www.linkedin.com/in/dominika-biedulska-8845881a4/" target="_blank">
       <img src="@/icons/linkedin.svg" alt="linkedin" class="bg-white rounded w-6 h-6" />
