111cb50974f20e8d3d091aedd5bc0747bff62764
DominoxB <dominika-biedulska@tlen.pl>
2023-07-20 14:29:36 +0200

    fix: change msg i18n

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,15 +2,10 @@ import { createI18n } from "vue-i18n"
 import en from "./en"
 import pl from "./pl"
 
-const messages = {
-  en,
-  pl
-}
-
 const i18n = createI18n({
   locale: "en",
   fallbackLocale: "pl",
-  messages
+  messages: { en, pl }
 })
 
 export default i18n
