1d2c38e53d28652af1ddac12bd6e005018f3b8d2
DominoxB <dominika-biedulska@tlen.pl>
2023-08-03 11:21:55 +0200

    feat: add link to icon

--- src/components/MySocials.vue
+++ src/components/MySocials.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="hidden pt-24 text-blue-200 font-semibold tracking-widest lg:block">
-    <div class="flex items-center gap-4">
-      <img src="@/icons/github.png" alt="github" class="w-8 h-8" />
+    <div class="flex">
       <a href="https://github.com/DominoxB" target="_blank">
-        <span class="">{{ $t('github') }}</span>
+        <div class="flex items-center gap-4">
+          <img src="@/icons/github.png" alt="github" class="w-8 h-8" />
+          <span class="">{{ $t('github') }}</span>
+        </div>
       </a>
     </div>
-    <div class="flex items-center gap-4 pt-6">
-      <img src="@/icons/linkedin.png" alt="github" class="w-8 h-8" />
+    <div class="flex pt-6">
       <a href="https://www.linkedin.com/in/dominika-biedulska" target="_blank">
-        <span class="">{{ $t('name') }}</span>
+        <div class="flex items-center gap-4">
+          <img src="@/icons/linkedin.png" alt="github" class="w-8 h-8" />
+          <span class="">{{ $t('name') }}</span>
+        </div>
       </a>
     </div>
   </div>
