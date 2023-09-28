0546715c0342c906652b6e38e6e4d88649bf1645
Dominika <dominika-biedulska@tlen.pl>
2023-09-28 15:48:02 +0200

    feat: add dist file

--- src/components/NameAndProfession.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1 class="font-medium text-5xl pb-5 tracking-wide xl:text-6xl">{{ $t('name') }}</h1>
-    <h2 class="text-2xl tracking-wider uppercase xl:text-3xl">{{ $t('job') }}</h2>
-  </div>
-</template>
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "./main.css"
-import i18n from './locales/index'
-
-createApp(App).use(i18n).mount('#app')
