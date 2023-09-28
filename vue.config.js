ab910a273cd07f2d2ea3f4a53556c5aef8d10248
Dominika <dominika-biedulska@tlen.pl>
2023-09-28 15:37:46 +0200

    fix: change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: "/my-portfolio/",
-  transpileDependencies: true
+  publicPath: "/my-portfolio/"
 })
