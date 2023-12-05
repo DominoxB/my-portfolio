46ad74180239ba1b0ecafd58c8f6e1ad2646fc7e
Dominika <dominika-biedulska@tlen.pl>
2023-12-05 09:04:14 +0100

    feat: rebuild component

--- src/components/ProfessionalExperience.vue
+++ src/components/ProfessionalExperience.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="font-semibold text-justify">
     <span class="uppercase font-semibold text-2xl">{{ $t('experience') }}</span>
-    <h3 class="mt-2">{{ $t('job') }}</h3>
-    <p class="italic">{{ $t('enterPreis') }}</p>
-    <p class="italic">{{ $t('jobDate') }}</p>
-    <p class="text-sm">{{ $t('jobDesc') }}
-    </p>
-    <h3 class="mt-8">{{ $t('mentoring') }}</h3>
-    <p class="italic">{{ $t('enterPreis') }}</p>
-    <p class="italic">{{ $t('mentoringDate') }}</p>
-    <p class="text-sm">{{ $t('mentoringDesc') }}
-    </p>
+    <h3 class="mt-2 text-xl">{{ $t('enterPreis') }} ({{ $t('jobDate') }})</h3>
+    <br>
+    <p class="italic">{{ $t('job') }}</p>
+    <p class="text-sm">{{ $t('jobDesc') }}</p>
+    <br>
+    <p class="italic">{{ $t('mentoring') }}</p>
+    <p class="text-sm">{{ $t('mentoringDesc') }}</p>
   </div>
 </template>
